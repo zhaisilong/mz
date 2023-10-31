49050be2c9d09adb1207170725a19e60e2f629f7
zhaisilong <zhaisilong@outlook.com>
2023-10-31 09:11:10 +0800

    improve

--- core/parser.py
+++ core/parser.py
@@ -1,14 +1,22 @@
-def parse_ions_block(block):
+from typing import List, Dict
+
+BEGIN_IONS = "BEGIN IONS"
+END_IONS = "END IONS"
+
+def parse_ions_block(block: Dict) -> Dict:
+    """
+    Parses a single ion block and returns a dictionary containing the metadata and masses.
+    """
     ions_data = {}
     masses = []
 
     lines = block.strip().split("\n")
     for line in lines:
-        if "BEGIN IONS" in line:
+        if BEGIN_IONS in line:
             continue
         if "=" in line:
-            key, value = line.split("=")
-            ions_data[key] = value
+            key, value = line.split("=", 1)
+            ions_data[key.strip()] = value.strip()  # Ensuring no unwanted spaces
         else:
             mz, intensity = map(float, line.split())
             masses.append((mz, intensity))
@@ -16,18 +24,24 @@ def parse_ions_block(block):
     ions_data["masses"] = masses
     return ions_data
 
-def parse_mgf(data):
-    blocks = data.strip().split("END IONS")
-    parsed_data = [parse_ions_block(block) for block in blocks if block.strip()]
+def parse_mgf(data: str) -> List[Dict]:
+    """
+    Parses the entire mgf data and returns a list of dictionaries, each representing an ion block.
+    """
+    blocks = data.strip().split(END_IONS)
+    return [parse_ions_block(block) for block in blocks if block.strip()]
 
-    return parsed_data
-
-def pick_mgf(mgf_list, target_mass, tense_threshold=100, digits=3):
-    selected_mgf = []
-    for peak in mgf_list:
-        for _mass, tense in peak["masses"]:
-            # 保证小数点后三位一致
-            if round(target_mass, digits) == round(_mass, digits) and tense > tense_threshold:
-                selected_mgf.append(peak)
-                break  # 一旦找到了匹配的mass，就跳出内部循环
-    return selected_mgf
+def pick_mgf(
+    mgf_list: List[Dict], target_mass: float, tense_threshold: float = 100, digits: int = 3
+) -> List[Dict]:
+    """
+    Selects ion blocks that have a mass matching the target_mass within the specified number of decimal places 
+    and with an intensity greater than the tense_threshold.
+    """
+    return [
+        peak for peak in mgf_list 
+        if any(
+            round(target_mass, digits) == round(_mass, digits) and tense > tense_threshold
+            for _mass, tense in peak["masses"]
+        )
+    ]
